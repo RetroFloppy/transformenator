63c46244a81a7618c77acf9eb9a5e9c7a0cc7f5e
David Schmidt <david__schmidt@users.sourceforge.net>
2013-03-07 16:55:50 +0000

    Update syntax

--- transformdirectories.sh
+++ transformdirectories.sh
@@ -2,13 +2,13 @@
 # Usage: transformdirectories.sh transform origindir destdir
 if [ "$2" != "" ]
 then
-export MY_HOME="`pwd`"
-cd $2
-for file in ./*
-do
-  java -jar $MY_HOME/transformenator.jar -t $MY_HOME/$1 $file > $3/$file.html
-done
+  export MY_HOME="`pwd`"
+  cd $2
+  for file in ./*
+  do
+    java -jar $MY_HOME/transformenator.jar -t $MY_HOME/$1 $file > $3/$file.txt
+  done
+  cd $MY_HOME
 else
-  echo "Please supply a directory name."
+  echo "Usage: transformdirectories.sh transform origindir destdir"
 fi
-cd $MY_HOME
