74b7939b2e229db67c3ad43d99765f7e5fa5b847
David Schmidt <david__schmidt@users.sourceforge.net>
2013-03-07 14:14:12 +0000

    Fix parm ordering

--- transformdirectories.bat
+++ transformdirectories.bat
@@ -1,2 +1,2 @@
-@rem Usage: transformdirectories.bat origindir destdir transform
-for /f "usebackq delims=|" %%f in (`dir /b "%1"`) do java -jar transformenator.jar -t %3 < "%1\%%f" > "%2\%%f.rtf"
+@rem Usage: transformdirectories.bat transform origindir destdir
+for /f "usebackq delims=|" %%f in (`dir /b "%2"`) do java -jar transformenator.jar -t %1 < "%2\%%f" > "%3\%%f.txt"
