943a422225ce3331ffc1656b11dd3a0fe19c02e9
David Schmidt <david__schmidt@users.sourceforge.net>
2013-06-06 01:13:01 +0000

    Add home directory specification

--- transformdirectory.bat
+++ transformdirectory.bat
@@ -1,2 +1,3 @@
 @rem Usage: transformdirectory.bat transform origindir destdir
-@for /f "usebackq delims=|" %%f in (`dir /b "%2"`) do java -jar transformenator.jar %1 "%2\%%f" "%3\%%f.txt"
+@rem Set TRANSFORM_HOME to the location of the transformenator.jar file.
+@for /f "usebackq delims=|" %%f in (`dir /b "%2"`) do java -jar %TRANSFORM_HOME%\transformenator.jar %1 "%2\%%f" "%3\%%f.rtf"
