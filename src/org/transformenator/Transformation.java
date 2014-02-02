51a7e839ff76643ec4d4ef98a45f20ef0088c657
David Schmidt <david__schmidt@users.sourceforge.net>
2014-02-02 22:02:59 +0000

    Complete-ish Leading Edge Word Processor (LEWP)

--- src/org/transformenator/Transformation.java
+++ src/org/transformenator/Transformation.java
@@ -1,6 +1,6 @@
 /*
  * Transformenator - perform transformation operations on binary files
- * Copyright (C) 2013 by David Schmidt
+ * Copyright (C) 2013 - 2014 by David Schmidt
  * david__schmidt at users.sourceforge.net
  *
  * This program is free software; you can redistribute it and/or modify it 
@@ -237,32 +237,41 @@ public class Transformation
 					 */
 					byte[] newBuf = new byte[inData.length];
 					int newBufCursor = 0;
-					for (int indexIndex = 0x414;indexIndex<0x500;indexIndex+=2)
+					boolean found9Yet = false;
+					for (int indexIndex = 0x400;indexIndex<0x500;indexIndex+=2)
 					{
 						int indexStart = UnsignedByte.intValue(inData[indexIndex],inData[indexIndex+1]);
-						if (indexStart == 65520)
-							break;
+						// System.out.println("Index start value: "+indexStart);
+						if ((indexStart >= 65520) || (indexStart < 9))
+							continue;
+						if (indexStart == 9)
+							found9Yet = true;
+						if (!found9Yet)
+							continue;
 						indexStart *= 512;
 						// System.err.println("Found index table 0x"+UnsignedByte.toString(inData[indexIndex+1])+UnsignedByte.toString(inData[indexIndex])+" at "+indexStart);
 						for (int i = indexStart; i < indexStart+256; i += 2)
 						{
 							int block = UnsignedByte.intValue(inData[i],inData[i+1]);
-							// System.err.print("byte ["+i+"] = "+UnsignedByte.toString(inData[i]));
-							// System.err.println(" byte ["+(i+1)+"] = "+UnsignedByte.toString(inData[i+1]));
 							int index = block * 512;
-							//System.err.println("block: 0x"+UnsignedByte.toString(inData[i+1])+UnsignedByte.toString(inData[i])
-							//		+" at file offset: 0x"+UnsignedByte.toString(UnsignedByte.hiByte(index))+UnsignedByte.toString(UnsignedByte.loByte(index)));
+							// System.err.println("block: 0x"+UnsignedByte.toString(inData[i+1])+UnsignedByte.toString(inData[i])
+							//	+" at file offset: 0x"+UnsignedByte.toString(UnsignedByte.hiByte(index))+UnsignedByte.toString(UnsignedByte.loByte(index)));
 							if (block == 0)
 								break;
 							if (block < 32768)
 							{
 								if (index + 1 < inData.length)
 								{
+									int j;
+									// Find out where the block really ends - remove trailing zeroes
+									for (j = index+511; j >= index; j--)
+										if (inData[j] != 0x00)
+											break;
+									// System.err.println("Found end of chunk at "+j+", or length "+(j-index)+".");
 									// Pull out the data in the chunk
-									for (int k = 0; k < 512; k++)
+									for (int k = 0; k < (j-index+1); k++)
 									{
-										if (inData[index + k] != 0x00)
-												newBuf[newBufCursor++] = inData[index + k];
+										newBuf[newBufCursor++] = inData[index + k];
 									}
 								}
 								else
