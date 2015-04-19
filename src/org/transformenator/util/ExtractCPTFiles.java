d0d4eb26fdd9c49477c27c6d73eb1a58f547ba11
David Schmidt <david__schmidt@users.sourceforge.net>
2015-04-19 02:46:48 +0000

    Clean up typos

--- src/org/transformenator/util/ExtractCPTFiles.java
+++ src/org/transformenator/util/ExtractCPTFiles.java
@@ -1,6 +1,6 @@
 /*
  * Transformenator - perform transformation operations on binary files
- * Copyright (C) 2014 by David Schmidt
+ * Copyright (C) 2014 - 2015 by David Schmidt
  * david__schmidt at users.sourceforge.net
  *
  * This program is free software; you can redistribute it and/or modify it 
@@ -31,7 +31,7 @@ import java.io.InputStream;
 import org.transformenator.Version;
 
 /*
- * ExtractWangFiles
+ * ExtractCPTFiles
  * 
  * This helper app pulls the files off of the virtual file system of CPT
  * word processor disks.
