2c4180cfe9112d95e42a71995ae7533e0d35ee20
RetroFloppySupport <32302105+RetroFloppySupport@users.noreply.github.com>
2018-04-10 03:11:32 -0400

    Correct placement of output buffer

--- src/org/transformenator/detanglers/LeadingEdge.java
+++ src/org/transformenator/detanglers/LeadingEdge.java
@@ -20,7 +20,7 @@ public class LeadingEdge extends ADetangler
 		for (int indexIndex = 0x400; indexIndex < 0x500; indexIndex += 2)
 		{
 			int indexStart = UnsignedByte.intValue(inData[indexIndex], inData[indexIndex + 1]);
-			// System.err.println("DEBUG: Index start value: "+indexStart);
+			// System.err.println("DEBUG: Index start value: 0x"+Integer.toHexString(indexStart)+" at indexIndex: 0x"+Integer.toHexString(indexIndex));
 			if ((indexStart >= 65520) || (indexStart < 9))
 				continue;
 			if (indexStart == 9)
@@ -28,17 +28,12 @@ public class LeadingEdge extends ADetangler
 			if (!found9Yet)
 				continue;
 			indexStart *= 512;
-			// System.err.println("DEBUG: Found index table
-			// 0x"+UnsignedByte.toString(inData[indexIndex+1])+UnsignedByte.toString(inData[indexIndex])+"
-			// at "+indexStart);
+			// System.err.println("DEBUG: Found index table 0x"+UnsignedByte.toString(inData[indexIndex+1])+UnsignedByte.toString(inData[indexIndex])+", pointing to table at 0x"+Integer.toHexString(indexStart));
 			for (int i = indexStart; i < indexStart + 256; i += 2)
 			{
 				int block = UnsignedByte.intValue(inData[i], inData[i + 1]);
 				int index = block * 512;
-				// System.err.println("DEBUG: block:
-				// 0x"+UnsignedByte.toString(inData[i+1])+UnsignedByte.toString(inData[i])
-				// +" at file offset:
-				// 0x"+UnsignedByte.toString(UnsignedByte.hiByte(index))+UnsignedByte.toString(UnsignedByte.loByte(index)));
+				// System.err.println("DEBUG: block: 0x"+UnsignedByte.toString(inData[i+1])+UnsignedByte.toString(inData[i]) +" at file offset: 0x"+UnsignedByte.toString(UnsignedByte.hiByte(index))+UnsignedByte.toString(UnsignedByte.loByte(index)));
 				if (block == 0)
 					break;
 				if (block < 32768)
@@ -62,11 +57,11 @@ public class LeadingEdge extends ADetangler
 						System.err.println("Found an index out of bounds: " + block);
 				}
 			}
-			inData = new byte[newBufCursor];
-			for (int i = 0; i < newBufCursor; i++)
-				inData[i] = newBuf[i];
-			// System.err.println("DEBUG: Data length after de-indexing: "+inData.length);
 		}
+		inData = new byte[newBufCursor];
+		for (int i = 0; i < newBufCursor; i++)
+			inData[i] = newBuf[i];
+		// System.err.println("DEBUG: Data length after de-indexing: "+inData.length);
 		return inData;
 	}
 
